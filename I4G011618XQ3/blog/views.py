a12359059709657e4f1039ea210e2926bcee5e53
Sophia Edeki <sophiesse143@gmail.com>
2022-06-21 19:28:50 +0100

    updated

--- I4G011618XQ3/blog/views.py
+++ I4G011618XQ3/blog/views.py
@@ -1,7 +1,32 @@
+from dataclasses import fields
+from pyexpat import model
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import ListView
+from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 from .models import Post
 # Create your views here.
 def PostListView(ListView):
     model = Post
+
+def PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+
+def PostDetailView(DetailView):
+    model = Post
+
+def PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
+    
+def PostDeleteView(DeleteView):
+    model = Post
+    fields = "__all__"
+    success_url = reverse_lazy("blog:all")
 
